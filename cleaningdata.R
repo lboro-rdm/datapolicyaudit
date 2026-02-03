b478f255d20bca737d40fb6081ebbccfb8a45026
lboro-rdm <rdm@lboro.ac.uk>
2026-02-03 16:56:39 +0000

    initial commit

---
+++ cleaningdata.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(janitor)
+
+
+# batch.csv was created on 2026-02-03, from public items in the Loughborough Research Repository'
+
+all_data <- read.csv("batch.csv")
+
+LB_data <- all_data %>%
+  clean_names() %>%
+  filter(item_type == "journal contribution") %>% 
+  filter(str_detect(school, 
+                    "Business and Economics|Loughborough Business School"))
+
+journal_counts <- filtered_data %>%
+  clean_names() %>%
+  filter(!is.na(issn) & issn != "") %>%
+  count(issn, published_in, sort = TRUE)
+
+write.csv(journal_counts, "business_journal_counts.csv", row.names = FALSE)
