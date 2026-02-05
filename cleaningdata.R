b8bc4c4af021fe200f0953ed5ebbba7dce2fb430
lboro-rdm <rdm@lboro.ac.uk>
2026-02-05 14:43:14 +0000

    updated workflow

--- cleaningdata.R
+++ cleaningdata.R
@@ -24,18 +24,59 @@ journal_counts <- LB_data %>%
     .groups = "drop"
   )
 
+write.csv(journal_counts_with_topics, "business_journal_counts.csv", row.names = FALSE)
+
+# Aeronautical, Automotive, Chemical and Materials Engineering -----------------
+
+aacme_data <- all_data %>%
+  clean_names() %>%
+  filter(item_type == "journal contribution") %>% 
+  filter(str_detect(school, 
+                    "Aeronautical, Automotive, Chemical and Materials Engineering"))
+
+journal_counts <- aacme_data %>%
+  filter(!is.na(issn) & issn != "") %>%
+  group_by(issn) %>%
+  summarise(
+    articles = n(),
+    journal_title = first(published_in),
+    .groups = "drop"
+  )
+
+write.csv(journal_counts_with_topics, "aacme_journal_counts.csv", row.names = FALSE)
+
+# Mechanical, Electrical and Manufacturing Engineering -----------------
+
+meme_data <- all_data %>%
+  clean_names() %>%
+  filter(item_type == "journal contribution") %>% 
+  filter(str_detect(school, 
+                    "Mechanical, Electrical and Manufacturing Engineering"))
+
+journal_counts <- meme_data %>%
+  filter(!is.na(issn) & issn != "") %>%
+  group_by(issn) %>%
+  summarise(
+    articles = n(),
+    journal_title = first(published_in),
+    .groups = "drop"
+  )
+
+write.csv(journal_counts_with_topics, "wolfson_journal_counts.csv", row.names = FALSE)
+
 # OpenAlex for subjects ---------------------------------------------------
 
 issns <- journal_counts %>%
   pull(1) 
 
+# journals_raw = Journals on OpenAlex
 journals_raw <- oa_fetch(
   entity = "sources",
   issn = issns,
   verbose = TRUE
 )
 
-journals_concepts <- journals_raw %>%
+journals_concepts <- journals_raw %>% 
   select(id, display_name, issn_l, issn, topics) 
 
 journals_topics_long <- journals_concepts %>%
@@ -57,27 +98,20 @@ journals_subfields_collapsed <- journals_subfields %>%
     .groups = "drop"
   )
 
+# journals_subfields_collapsed = Journals with subfields
+
 journal_counts_with_topics <- journal_counts %>%
   left_join(
     journals_subfields_collapsed,
     by = c("issn" = "issn_l")
   ) %>% 
-  filter(!is.na(subfields) & subfields != "")
+  filter(!is.na(subfields) & subfields != "") %>%
+  select(
+    journal_title.y,   # from your data
+    issn,
+    articles,
+    subfields
+  ) %>%
+  arrange(desc(articles))
 
-
-write.csv(journal_counts_with_topics, "business_journal_counts.csv", row.names = FALSE)
-
-# Science -----------------------------------------------------------------
-
-science_data <- all_data %>%
-  clean_names() %>%
-  filter(item_type == "journal contribution") %>% 
-  filter(str_detect(school, 
-                    "Science") | str_detect(department, "Science"))
-
-journal_counts <- science_data %>%
-  clean_names() %>%
-  filter(!is.na(issn) & issn != "") %>%
-  count(issn, published_in, sort = TRUE)
-
-write.csv(journal_counts, "science.csv", row.names = FALSE)
+# journal_counts_with_topics = Unique entries
