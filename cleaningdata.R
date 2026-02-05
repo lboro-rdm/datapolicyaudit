46d3295221217585c3ec41d1a83f211dbe540509
lboro-rdm <rdm@lboro.ac.uk>
2026-02-05 12:06:36 +0000

    final workflow complete

--- cleaningdata.R
+++ cleaningdata.R
@@ -16,28 +16,13 @@ LB_data <- all_data %>%
                     "Business and Economics|Loughborough Business School"))
 
 journal_counts <- LB_data %>%
-  clean_names() %>%
   filter(!is.na(issn) & issn != "") %>%
-  count(issn, published_in, sort = TRUE)
-
-write.csv(journal_counts, "business_journal_counts.csv", row.names = FALSE)
-
-
-# Science -----------------------------------------------------------------
-
-science_data <- all_data %>%
-  clean_names() %>%
-  filter(item_type == "journal contribution") %>% 
-  filter(str_detect(school, 
-                    "Science") | str_detect(department, "Science"))
-
-journal_counts <- science_data %>%
-  clean_names() %>%
-  filter(!is.na(issn) & issn != "") %>%
-  count(issn, published_in, sort = TRUE)
-
-write.csv(journal_counts, "science.csv", row.names = FALSE)
-
+  group_by(issn) %>%
+  summarise(
+    articles = n(),
+    journal_title = first(published_in),
+    .groups = "drop"
+  )
 
 # OpenAlex for subjects ---------------------------------------------------
 
@@ -58,9 +43,41 @@ journals_topics_long <- journals_concepts %>%
   unnest(topics, names_sep = "_")
 
 journals_subfields <- journals_topics_long %>%
-  filter(topics_type == "subfield") %>%   # keep only subfields
+  filter(topics_type == "subfield") %>%
   select(
-    display_name, 
-    issn_l, 
-    topics_display_name
+    journal_title = display_name,
+    issn_l,
+    subfield = topics_display_name
   )
+
+journals_subfields_collapsed <- journals_subfields %>%
+  group_by(journal_title, issn_l) %>%
+  summarise(
+    subfields = paste(sort(unique(subfield)), collapse = ", "),
+    .groups = "drop"
+  )
+
+journal_counts_with_topics <- journal_counts %>%
+  left_join(
+    journals_subfields_collapsed,
+    by = c("issn" = "issn_l")
+  ) %>% 
+  filter(!is.na(subfields) & subfields != "")
+
+
+write.csv(journal_counts_with_topics, "business_journal_counts.csv", row.names = FALSE)
+
+# Science -----------------------------------------------------------------
+
+science_data <- all_data %>%
+  clean_names() %>%
+  filter(item_type == "journal contribution") %>% 
+  filter(str_detect(school, 
+                    "Science") | str_detect(department, "Science"))
+
+journal_counts <- science_data %>%
+  clean_names() %>%
+  filter(!is.na(issn) & issn != "") %>%
+  count(issn, published_in, sort = TRUE)
+
+write.csv(journal_counts, "science.csv", row.names = FALSE)
